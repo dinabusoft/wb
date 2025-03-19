c1578837d0faa4f1f87dede7f641de3c82dcf66b
Dina Utami <dinabusoft@gmail.com>
2025-03-20 01:29:24 +0700

    first commit

---
+++ resources/js/Plugins/vuetify.js
@@ -0,0 +1,51 @@
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const customeTheme = {
+    dark: false,
+    colors: {
+        vertical_nav: "#FFFFFF",
+        horizontal_nav: "#071952",
+        background: "#EDEDED",
+        form_background: "FAFAFA",
+        primary: "#088395",
+        secondary: "#1F6180",
+        accent: "#37B7C3",
+        error: "#000000",
+        cards: "#FFFFFF",
+        success: "#4CAF50",
+        warning: "#FFC107",
+        lightblue: "#14c6FF",
+        yellow: "#FFCF00",
+        pink: "#FF1976",
+        orange: "#FF8657",
+        magenta: "#C33AFC",
+        darkblue: "#1E2D56",
+        gray: "#909090",
+        neutralgray: "#9BA6C1",
+        green: "#2ED47A",
+        red: "#FF5c4E",
+        darkblueshade: "#308DC2",
+        lightgray: "#BDBDBD",
+        lightpink: "#FFCFE3",
+        white: "#FFFFFF",
+        muted: "#6c757d",
+    },
+};
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        defaultTheme: "customeTheme",
+        themes: {
+            customeTheme,
+        },
+    },
+    
+});
+
+export default vuetify;
